07092322ac5e1e8fd969735fc847cea3a29e3f04
Artem Krivonozhko <artemkrivonozhko@gmail.com>
2022-02-03 18:06:40 +0300

    Add straight-forward implementation

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,11 @@
 class CarsController < ApplicationController
+  def mileage
+    render json: { mileage: car_resource.fetch_mileage.to_s }
+  end
+
+  private
+
+  def car_resource
+    @car ||= Car.find(params[:id])
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,13 @@
 class Car < ApplicationRecord
+  def fetch_mileage
+    MileageFetcher.new.perform
+  end
+
+  class MileageFetcher
+    def perform
+      sleep 3
+
+      9000
+    end
+  end
 end
